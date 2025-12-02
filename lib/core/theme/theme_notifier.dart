c9b40131f637b34cffdc7745565b72cb018a1890
Luuk <0510luuk@gmail.com>
2025-12-02 20:13:33 +0100

    Update theme notifier
    Thema-keuze wordt nu bewaard:
    
    ThemeNotifier laadt bij constructie opgeslagen modus/OLED-flag uit SharedPreferences en meldt veranderingen.
    Nieuwe keuzes worden meteen opgeslagen (mode + OLED-flag).

--- lib/core/theme/theme_notifier.dart
+++ lib/core/theme/theme_notifier.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier extends ChangeNotifier {
+  ThemeNotifier() {
+    _load();
+  }
+
   ThemeMode _mode = ThemeMode.system;
   bool _useOledDark = true;
 
@@ -12,6 +17,38 @@ class ThemeNotifier extends ChangeNotifier {
     if (useOledDark != null) {
       _useOledDark = useOledDark;
     }
+    _persist();
     notifyListeners();
   }
+
+  Future<void> _load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final storedMode = prefs.getString(_modeKey);
+    final storedOled = prefs.getBool(_oledKey);
+    if (storedMode != null) {
+      switch (storedMode) {
+        case 'light':
+          _mode = ThemeMode.light;
+          break;
+        case 'dark':
+          _mode = ThemeMode.dark;
+          break;
+        default:
+          _mode = ThemeMode.system;
+      }
+    }
+    if (storedOled != null) {
+      _useOledDark = storedOled;
+    }
+    notifyListeners();
+  }
+
+  Future<void> _persist() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_modeKey, _mode.name);
+    await prefs.setBool(_oledKey, _useOledDark);
+  }
+
+  static const String _modeKey = 'theme_mode';
+  static const String _oledKey = 'theme_oled';
 }
