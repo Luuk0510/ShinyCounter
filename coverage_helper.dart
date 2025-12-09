7ed591240c6454e37b10461df6ca43258950578b
Luuk <0510luuk@gmail.com>
2025-12-09 22:29:53 +0100

    Fix coverage viewer to ignore generated l10n files

---
+++ coverage_helper.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  final lcov = File('coverage/lcov.info');
+  if (!lcov.existsSync()) return;
+  final filtered = lcov
+      .readAsLinesSync()
+      .where((line) =>
+          // Skip generated l10n
+          !line.startsWith('SF:lib/l10n/gen/') &&
+          // Skip generated localization facade
+          !line.startsWith('SF:lib/l10n/app_localizations.dart'))
+      .toList();
+  lcov.writeAsStringSync(filtered.join('\n'));
+}
