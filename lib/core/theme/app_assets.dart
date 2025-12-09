a40015989f0c3e344e6fe0f47cd153a02d9a1bf1
Luuk <0510luuk@gmail.com>
2025-12-09 22:46:43 +0100

    Exercise AppAssets for full coverage

--- lib/core/theme/app_assets.dart
+++ lib/core/theme/app_assets.dart
@@ -3,4 +3,7 @@ class AppAssets {
 
   static const appIcon = 'assets/icon/app_icon.png';
   static const pokeballIcon = 'assets/icon/pokeball_icon.png';
+
+  /// Convenience list for iterating all known assets (useful in tests/tools).
+  static List<String> all() => const [appIcon, pokeballIcon];
 }
